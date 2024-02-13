726f71e8d994851f00d4f2613569c1162aa99d78
sophiajw <sophia.wagner@t-online.de>
2024-02-13 01:33:43 +0100

    move parsing to options.py

--- dinov2/eval/miccai/extract_slide_features.py
+++ dinov2/eval/miccai/extract_slide_features.py
@@ -12,242 +12,79 @@ import numpy as np
 import pandas as pd
 import slideio
 import torch
+import yaml
 from dataset import SlideDataset
 from models.return_model import get_models, get_transforms
+from options import Options
 from PIL import Image
 # import cv2
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from utils import (bgr_format, get_driver, get_scaling, save_hdf5,
-                   save_qupath_annotation, save_tile_preview, threshold)
+from utils import bgr_format, get_driver, get_scaling, save_hdf5, threshold
 
-parser = argparse.ArgumentParser(description="Feature extraction")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# 20-041_K6_vK
-# 20-069_K3_vK
-# 20-009_K6_vK
-
-parser.add_argument(
-    "--slide_path",
-    help="path of slides to extract features from",
-    default="/mnt/ceph_vol/raw_data/2021",
-    type=str,
-)
-parser.add_argument(
-    "--save_path",
-    help="path to save everything",
-    default="/mnt/ceph_vol/features/test/",
-    type=str,
-)
-parser.add_argument(
-    "--file_extension",
-    help="file extension the slides are saved under, e.g. tiff",
-    default=".czi",
-    type=str,
-)
-parser.add_argument(
-    "--model",
-    help="select model ctranspath, retccl, all",
-    nargs="+",
-    default="dinov2_finetuned",
-    type=str,
-)
-parser.add_argument(
-    "--checkpoints",
-    help="path to checkpoint",
-    nargs="+",
-    default=["/mnt/ceph_vol/dinov2/dinov2/debug/logs/teacher_checkpoint.pth"],
-    type=str,
-)
-parser.add_argument(
-    "--scene_list",
-    help="list of scene(s) to be extracted",
-    nargs="+",
-    default=[0],
-    type=int,
-)
-parser.add_argument("--patch_size", help="Patch size for saving", default=512, type=int)
-parser.add_argument(
-    "--white_thresh",
-    help="if all RGB pixel values are larger than this value, the pixel is considered as white/background",
-    default=[175, 190, 178],
-    nargs='+',
-    type=int,
-)
-parser.add_argument(
-    "--black_thresh",
-    help="if all RGB pixel values are smaller or equal than this value, the pixel is considered as black/background",
-    default=0,
-    type=str,
-)
-parser.add_argument(
-    "--invalid_ratio_thresh",
-    help="maximum acceptable amount of background",
-    default=0.3,
-    type=float,
-)
-parser.add_argument(
-    "--edge_threshold",
-    help="canny edge detection threshold. if smaller than this value, patch gets discarded",
-    default=1,
-    type=int,
-)
-parser.add_argument(
-    "--resolution_in_mpp",
-    help="resolution in mpp, usually 10x= 1mpp, 20x=0.5mpp, 40x=0.25, ",
-    default=1,
-    type=float,
-)
-parser.add_argument(
-    "--downscaling_factor",
-    help="only used if >0, overrides manual resolution. needed if resolution not given",
-    default=0,
-    type=float,
-)
-parser.add_argument(
-    "--save_tile_preview",
-    help="set True if you want nice pictures",
-    action='store_true',
-)
-
-parser.add_argument(
-    "--save_patch_images",
-    help="True if each patch should be saved as an image",
-    action='store_true',
-)
-parser.add_argument(
-    "--preview_size", help="size of tile_preview", default=4096, type=int
-)
-parser.add_argument("--batch_size", default=256, type=int)
-parser.add_argument(
-    "--exctraction_list",
-    help="if only a subset of the slides should be extracted save their names in a csv",
-    default=None, #"/home/ubuntu/idkidc/extraction_list.csv"
-    type=str,
-)  #
-parser.add_argument(
-    "--save_qupath_annotation",
-    help="set True if you want nice qupath annotations",
-    default=False,
-    type=bool,
-)
-parser.add_argument(
-    "--calc_thresh",
-    help="darker colours than this are considered calc",
-    default=[40, 40, 40],
-    nargs='+',
-    type=int,
-)
-parser.add_argument(
-    "--split",
-    help="(k,n): split slides into n distinct chunks and process number k. (0,1) for all slides at once. E.g. one urn with (0,2) and one with (1,2) to split data.",
-    default=[0, 1],
-    nargs='+',
-    type=int,
-)
+"""
+extracts features from slides of specified dataset with given checkpoints
+"""
 
 
 def main(args):
-    """
-    Args:
-    args: argparse.Namespace, containing the following attributes:
-    - slide_path (str): Path to the slide files.
-    - save_path (str): Path where to save the extracted features.
-    - file_extension (str): File extension of the slide files (e.g., '.czi').
-    - models (list): List of models to use for feature extraction.
-    - scene_list (list): List of scenes to process.
-    - save_patch_images (bool): Whether to save each patch as an image.
-    - patch_size (int): Size of the image patches to process.
-    - white_thresh (int): Threshold for considering a pixel as white/background (based on RGB values).
-    - black_thresh (int): Threshold for considering a pixel as black/background (based on RGB values).
-    - invalid_ratio_thresh (float): Threshold for invalid ratio in patch images.
-    - edge_threshold (int): Canny edge detection threshold. Patches with values smaller than this are discarded.
-    - resolution_in_mpp (float): Resolution in microns per pixel (e.g., 10x=1mpp, 20x=0.5mpp, 40x=0.25).
-    - downscaling_factor (float): Downscaling factor for the images; used if >0, overrides manual resolution.
-    - save_tile_preview (bool): Set to True if you want to save tile preview images.
-    - preview_size (int): Size of tile_preview images.
-    - extraction_list (str): Path to csv file containing the list of slides to be extracted (optional).
-    - save_qupath_annotation (bool): Set to True if you want to save nice QuPath annotations (optional).
-
-    Returns:
-    None
-    """
-
-    # Set device to GPU if available, else CPU
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    # adapt the feature directory in data_config
+    with open(args.data_config, 'r') as f:
+        data_config = yaml.safe_load(f)
+        slide_path = data_config[args.dataset]['slide_dir']
 
     # Get slide files based on the provided path and file extension
-    slide_files = sorted(Path(args.slide_path).glob(f"**/*{args.file_extension}"))
+    slides = sorted(Path(slide_path).glob(f"**/*{args.file_extension}"))
 
     if bool(args.exctraction_list) is not False:
         to_extract = pd.read_csv(args.exctraction_list).iloc[:, 0].tolist()
-        slide_files = [file for file in slide_files if file.name in to_extract]
+        slides = [file for file in slides if file.name in to_extract]
 
     # filter out slide files using RegEx
     #slide_files = sorted([
     #    file for file in slide_files if re.search("-DX", str(file))
     #])
-    chunk_len=math.ceil(len(slide_files)/args.split[1])
+    chunk_len=math.ceil(len(slides)/args.split[1])
     start=args.split[0]*chunk_len
-    end=min(start+chunk_len,len(slide_files))
-    slide_files=slide_files[start:end]
-    model_dicts=[]
-    # Get model dictionaries
-    for c in args.checkpoints:
-        save_path=Path(c).parent/Path(args.slide_path).name
-        model_dicts.append({"model":get_models(args.model, c), "name": args.model, "transforms": get_transforms(args.model),"save_path": save_path})
-        output_path = Path(save_path) / "h5_files"
-        output_path.mkdir(parents=True, exist_ok=True)
+    end=min(start+chunk_len,len(slides))
+    slides=slides[start:end]
+
     # Get the driver for the slide file extension
-        
     driver = get_driver(args.file_extension)
 
-    # Process models
-    for model in model_dicts:
-        model_name = model["name"]
-        save_path = model["save_path"]
-        save_dir = (
-            Path(save_path)
-            / "h5_files"
-            / f"{args.patch_size}px_{model_name}_{args.resolution_in_mpp}mpp_{args.downscaling_factor}xdown_normal"
-        )
+    # Load models for checkpoints
+    model_dicts = []
+    checkpoints = Path(args.run).rglob("**/*teacher_checkpoint.pth") if Path(args.run).is_dir() else [Path(args.run)]
+    args.run = args.run if Path(args.run).is_dir() else str(Path(args.run).parent.parent.parent)
+    
+    for i, c in enumerate(checkpoints):
+        output_dir = Path(args.run) / "features" / f"{args.dataset}_{c.stem}_{args.patch_size}px_{args.model[i]}_{args.resolution_in_mpp}mpp_{args.downscaling_factor}xdown_normal"
+        output_dir.mkdir(parents=True, exist_ok=True)
+        model_dicts.append({
+            "model": get_models(args.model[i], c), 
+            "name": args.model[i], 
+            "transforms": get_transforms(args.model[i]),
+            "save_path": output_dir
+        })
 
-        # Create save directory for the model
-        save_dir.mkdir(parents=True, exist_ok=True)
-
-        # Create a dictionary of argument names and values
+        # save config
         arg_dict = vars(args)
-
-        # Write the argument dictionary to a text file
-        with open(save_dir / "config.yml", "w") as f:
+        with open(output_dir / "config.yaml", "w") as f:
             for arg_name, arg_value in arg_dict.items():
-                f.write(f"{arg_name}: {arg_value}\n")
+                if isinstance(arg_value, list):
+                    f.write(f"{arg_name}: {arg_value[i]}\n")
+                else: 
+                    f.write(f"{arg_name}: {arg_value}\n")
 
-    # Create directories
-    if args.save_tile_preview:
-        tile_path = (
-            Path(save_path)
-            / f"tiling_previews_{args.patch_size}px_{args.resolution_in_mpp}mpp_{args.downscaling_factor}xdown_normal"
-        )
-        tile_path.mkdir(parents=True, exist_ok=True)
-    else:
-        tile_path = None
-
-    if args.save_qupath_annotation:
-        annotation_path = (
-            Path(save_path)
-            / f"qupath_annotation_{args.patch_size}px_{args.resolution_in_mpp}mpp_{args.downscaling_factor}xdown_normal"
-        )
-        annotation_path.mkdir(parents=True, exist_ok=True)
-    else:
-        annotation_path = None
     # Process slide files
     start = time.perf_counter()
-    for slide_file in tqdm(slide_files, position=0, leave=False, desc="slides"):
+    for slide_file in tqdm(slides, position=0, leave=False, desc="slides"):
         slide = slideio.Slide(str(slide_file), driver)
         slide_name = slide_file.stem
         extract_features(
-            slide, slide_name, model_dicts, device, args, tile_path, annotation_path
+            output_dir, slide, slide_name, model_dicts, DEVICE, args
         )
 
     end = time.perf_counter()
@@ -329,13 +166,12 @@ def patches_to_feature(
 
 
 def extract_features(
+    save_dir: str,
     slide: slideio.py_slideio.Slide,
     slide_name: str,
     model_dicts: list[dict],
     device: torch.device,
     args: argparse.Namespace,
-    tile_path: str,
-    annotation_path: str,
 ):
     """
     Extract features from a slide using a given model.
@@ -347,8 +183,6 @@ def extract_features(
         model_dict (dict): Dictionary containing the model, transforms, and model name.
         scene_list (list): List of scenes to process.
         device (torch.device): Device to perform computations on (CPU or GPU).
-        tile_path (pathlib.Path): A Path object representing the path where the tile preview image will be saved.
-        annotation_path (pathlib.Path): The path to the output directory.
 
     Returns:
         None
@@ -422,18 +256,13 @@ def extract_features(
             for key in patch_feats.keys():
                 feats[key].extend(patch_feats[key])
 
-        # saves tiling preview on slide in desired size
-        if args.save_tile_preview:
-            save_tile_preview(args, slide_name, scn, wsi, coords, tile_path)
-
-        if args.save_qupath_annotation:
-            save_qupath_annotation(args, slide_name, scn, coords, annotation_path)
-
     # Write data to HDF5
     if len(model_dicts)>0:
-        save_hdf5(args, slide_name, coords, feats, orig_sizes,scalings,model_dicts)
+        save_hdf5(save_dir, args, slide_name, coords, feats, orig_sizes,scalings, model_dicts)
 
 
 if __name__ == "__main__":
-    args = parser.parse_args()
+    parser = Options()
+    args = parser.parse()
+
     main(args)
---
+++ dinov2/eval/miccai/options.py
@@ -0,0 +1,40 @@
+import argparse
+
+
+class Options:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="Feature extraction")
+
+        # model
+        self.parser.add_argument("--model", help="select model ctranspath, retccl, all", nargs="+", default="dinov2_finetuned", type=str)
+        self.parser.add_argument("--run", help="path to folder of run", type=str)
+        self.parser.add_argument("--data_config", help="path to config file for data paths", default="dinov2/eval/slide_level/configs/data_config.yaml", type=str)
+        self.parser.add_argument("--dataset", help="name of dataset", default="TCGA-CRC", type=str)
+        # size and resolution
+        self.parser.add_argument("--patch_size", help="Patch size for saving", default=512, type=int)
+        self.parser.add_argument("--resolution_in_mpp", help="resolution in mpp, usually 10x= 1mpp, 20x=0.5mpp, 40x=0.25, ", default=1, type=float)
+        self.parser.add_argument("--downscaling_factor", help="only used if >0, overrides manual resolution. needed if resolution not given", default=0, type=float)
+        
+        # tissue segmentation
+        self.parser.add_argument("--white_thresh", help="if all RGB pixel values are larger than this value, the pixel is considered as white/background", default=[175, 190, 178], nargs='+', type=int)
+        self.parser.add_argument("--black_thresh", help="if all RGB pixel values are smaller or equal than this value, the pixel is considered as black/background", default=0, type=str)
+        self.parser.add_argument("--invalid_ratio_thresh", help="maximum acceptable amount of background", default=0.3, type=float)
+        self.parser.add_argument("--edge_threshold", help="canny edge detection threshold. if smaller than this value, patch gets discarded", default=1, type=int)
+        self.parser.add_argument("--calc_thresh", help="darker colours than this are considered calc", default=[40, 40, 40], nargs='+', type=int)
+
+        # other options
+        self.parser.add_argument("--file_extension", help="file extension the slides are saved under, e.g. tiff", default=".svs", type=str)
+        self.parser.add_argument("--scene_list", help="list of scene(s) to be extracted", nargs="+", default=[0], type=int)
+        self.parser.add_argument("--save_tile_preview", help="set True if you want nice pictures", action='store_true')
+        self.parser.add_argument("--save_patch_images", help="True if each patch should be saved as an image", action='store_true')
+        self.parser.add_argument("--preview_size", help="size of tile_preview", default=4096, type=int)
+        self.parser.add_argument("--batch_size", default=256, type=int)
+        self.parser.add_argument("--exctraction_list", help="if only a subset of the slides should be extracted save their names in a csv", default=None, type=str)  #
+        self.parser.add_argument("--save_qupath_annotation", help="set True if you want nice qupath annotations", default=False, type=bool)
+        self.parser.add_argument("--split", help="(k,n): split slides into n distinct chunks and process number k. (0,1) for all slides at once. E.g. one urn with (0,2) and one with (1,2) to split data.", default=[0, 1], nargs='+', type=int)
+
+    def parse(self):
+        self.opt = self.parser.parse_args()
+        args = vars(self.opt)
+
+        return self.opt
