7d33f9bfed865295d2b429aa8c4c4246831bd5e8
Valentin Koch <mail@valentin.koch.de>
2024-02-16 10:47:35 +0100

    minor refactor

---
+++ dinov2/eval/miccai/scripts/feature_extract_and_eval_all_patch_level.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH -o slurm_outputs/eval_all_ctran.txt
+#SBATCH -e slurm_outputs/eval_all_error_ctran.txt
+#SBATCH -J feature_ex
+#SBATCH -p gpu_p
+#SBATCH -c 18
+#SBATCH --mem=150G
+#SBATCH -q gpu_long
+#SBATCH --time=5:00:00
+#SBATCH --nice=10000
+#SBATCH --gres=gpu:1
+
+# Environment setup
+source $HOME/.bashrc
+
+# choose the right directory (Valentin: /home/icb/valentin.koch/dinov2, Sophia: /home/haicu/sophia.wagner/projects/dinov2)    
+cd /home/icb/valentin.koch/dinov2
+
+# activate your own conda environment (Valentin: feature_ex, Sophia: vim)
+conda activate feature_ex
+
+# set checkpoint to evaluate as input 
+python dinov2/eval/miccai/extract_and_eval_patches.py --run_path "/lustre/groups/shared/users/peng_marr/HistoDINO/logs/baseline_models/retccl" --model_name resnet50  --image_path_train /home/icb/valentin.koch/dinov2/dinov2/eval/miccai/matek_train.csv --image_path_test "/home/icb/valentin.koch/dinov2/dinov2/eval/miccai/matek_test.csv" --experiment_name matek  --num_workers 16 
+
+
