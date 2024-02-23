c0b96fd05da54d5777167a26ac9c535a08fc94f4
sophiajw <sophia.wagner@t-online.de>
2024-02-23 04:39:46 +0100

    update scripts

--- dinov2/eval/miccai/scripts/feature_extract_and_eval_all_patch_level.sh
+++ dinov2/eval/miccai/scripts/feature_extract_and_eval_all_patch_level.sh
@@ -14,11 +14,12 @@
 # Environment setup
 source $HOME/.bashrc
 
-# choose the right directory (Valentin: /home/icb/valentin.koch/dinov2, Sophia: /home/haicu/sophia.wagner/projects/dinov2)    
-cd /home/icb/valentin.koch/dinov2
+# choose the right directory 
+cd /home/haicu/sophia.wagner/projects/dinov2  
+# cd /home/icb/valentin.koch/dinov2
 
 # activate your own conda environment (Valentin: feature_ex, Sophia: vim)
-conda activate feature_ex
+conda activate dinov2
 
 # set checkpoint to evaluate as input 
 python dinov2/eval/miccai/extract_and_eval_patches.py --run_path "/lustre/groups/shared/users/peng_marr/HistoDINO/logs/baseline_models/retccl" --model_name resnet50  --image_path_train /home/icb/valentin.koch/dinov2/dinov2/eval/miccai/matek_train.csv --image_path_test "/home/icb/valentin.koch/dinov2/dinov2/eval/miccai/matek_test.csv" --experiment_name matek  --num_workers 16 
